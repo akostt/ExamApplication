8219d7f32df03176c7ec6371e66ef1f067de455f
stu-ipsp123 <stu-ipsp123@AMDVLA3511GW07>
2026-07-02 09:28:44 +0300

    InitialCommit

---
+++ src/ExamApplication/Flight.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamApplication
+{
+    class Flight
+    {
+        public string AuroportOut { get; set; }
+
+        public string AeroportIn { get; set; }
+    }
+}
